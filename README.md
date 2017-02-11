3672fea153a22894348a92dd6f19d018e65ce078
Programster <sdpagent@gmail.com>
2017-02-11 14:17:01 +0000

    Import of working code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # node-chat-example
 A simple node chat example based on [a post I read on codeproject.com](https://www.codeproject.com/Articles/871622/Writing-a-Chat-Server-using-Node-js-TypeScript-and)
+
+
+## How To Run
+Make sure you have installed [NodeJs](http://blog.programster.org/install-nodejs/).
+
+Go to the server folder and execute:
+```
+npm start
+```
+
+Go to the client folder and double click index.html to open it in your browser.
+
+Press f12 to look at the console. You should see messages.
---
+++ src/server/index.ts
@@ -0,0 +1,29 @@
+
+'use strict';
+
+import WebSocket = require('ws');
+import models = require('./models');
+
+var port: number = process.env.PORT || 3000;
+var WebSocketServer = WebSocket.Server;
+var server = new WebSocketServer({ port: port });
+
+server.on('connection', ws => {
+    ws.on('message', message => {
+        try {
+            var userMessage: models.UserMessage = new models.UserMessage(message);
+            broadcast(JSON.stringify(userMessage));
+        } catch (e) {
+            console.error(e.message);
+        }
+    });
+});
+
+function broadcast(data: string): void {
+    console.log("Broadcasting: " + data);
+    server.clients.forEach(client => {
+        client.send(data);
+    }); 
+};
+
+console.log('Server is running on port', port);
